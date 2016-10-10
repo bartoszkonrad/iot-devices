03aa23a51362fb732ef276f32c30ffda9a3c4095
bartoszkonrad <bartosz.konrad@gmail.com>
2016-10-11 00:12:55 +0200

    poc

--- leds/src/leds.ino
+++ leds/src/leds.ino
@@ -3,17 +3,18 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const byte bdrBtnPin = D1;
-const byte lvrBtnPin = D2;
-
-const char* ssid = "kolasz_iot";
 const char* password = "neV3na4E";
+const char* ssid = "kolasz_iot";
 
-const byte lvrPin0 = D6;
-const byte lvrPin1 = D7;
+const byte kitchenPin = D5;
+const byte rgbPin = D6;
+const byte bathroomPin = D7;
 
-const byte bdrPin0 = D5;
-const byte bdrPin1 = D0;
+const byte atxPowerPin = D1;
+const byte atxStatePin = A0;
+
+int bathroomPWM = 0;
+int kitchenPWM = 0;
 
 ESP8266WebServer server(80);
 
@@ -21,17 +22,16 @@ void setup() {
   Serial.begin(9600);
   Serial.println("IoT");
 
-  pinMode(lvrPin0, OUTPUT);
-  pinMode(lvrPin1, OUTPUT);
+  pinMode(kitchenPin, OUTPUT);
+  pinMode(rgbPin, OUTPUT);
+  pinMode(bathroomPin, OUTPUT);
+  pinMode(atxPowerPin, OUTPUT);
+  pinMode(atxStatePin, INPUT);
 
-  pinMode(bdrPin0, OUTPUT);
-  pinMode(bdrPin1, OUTPUT);
-
-  digitalWrite(lvrPin0, HIGH);
-  digitalWrite(lvrPin1, HIGH);
-
-  digitalWrite(bdrPin0, HIGH);
-  digitalWrite(bdrPin1, HIGH);
+  digitalWrite(kitchenPin, LOW);
+  digitalWrite(rgbPin, LOW);
+  digitalWrite(bathroomPin, LOW);
+  digitalWrite(atxPowerPin, HIGH);
 
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -51,35 +51,7 @@ void setup() {
   }
 
   server.on("/", handleRoot);
-  server.on("/light", handleLight);
-
-  server.on("/loff", [](){
-    server.send(200, "text/plain", "lON");
-    Serial.println("ON");
-    digitalWrite(lvrPin0, HIGH);
-    digitalWrite(lvrPin1, HIGH);
-  });
-
-  server.on("/lon", [](){
-    server.send(200, "text/plain", "lOFF");
-    Serial.println("OFF");
-    digitalWrite(lvrPin0, LOW);
-    digitalWrite(lvrPin1, LOW);
-  });
-
-  server.on("/boff", [](){
-    server.send(200, "text/plain", "bON");
-    Serial.println("ON");
-    digitalWrite(bdrPin0, HIGH);
-    digitalWrite(bdrPin1, HIGH);
-  });
-
-  server.on("/bon", [](){
-    server.send(200, "text/plain", "bOFF");
-    Serial.println("OFF");
-    digitalWrite(bdrPin0, LOW);
-    digitalWrite(bdrPin1, LOW);
-  });
+  server.on("/leds", handleLeds);
 
   server.onNotFound(handleNotFound);
 
@@ -93,19 +65,24 @@ void loop() {
 
 void handleRoot() {
   String payload = "";
-  payload += "\nState logic inverted (HIGH means OFF and so on)";
-  payload += "\nbdr0: ";
-  payload += String(digitalRead(bdrPin0));
-  payload += "\nbdr1: ";
-  payload += String(digitalRead(bdrPin1));
-  payload += "\nlvr0: ";
-  payload += String(digitalRead(lvrPin0));
-  payload += "\nlvr1: ";
-  payload += String(digitalRead(lvrPin1));
+  payload += "kitchen: ";
+  payload += String(kitchenPWM);
+  payload += "\nbathroom: ";
+  payload += String(bathroomPWM);
+  payload += "\nrgb: ";
+  payload += String(digitalRead(rgbPin));
+  payload += "\natx power: ";
+  payload += String(digitalRead(atxPowerPin));
+  payload += "\natx state: ";
+  payload += String(analogRead(atxStatePin));
   payload += "\n";
-  payload += "<a href=\"http://rooms.lan/light?bdr=1\">Turn ON bedroom</a>";
+  payload += "\"http://leds.lan/leds?kitchen=0\" 0-1023";
   payload += "\n";
-  payload += "<a href=\"http://rooms.lan/light?bdr=0\">Turn OFF bedroom</a>";
+  payload += "\"http://leds.lan/leds?bathroom=0\" 0-1023";
+  payload += "\n";
+  payload += "\"http://leds.lan/leds?rgb=0\" 0/1";
+  payload += "\n";
+  payload += "\"http://leds.lan/leds?atx=0\" 0/1";
   server.send(200, "text/plain", payload);
   Serial.println("root");
 }
@@ -125,65 +102,53 @@ void handleNotFound(){
   server.send(404, "text/plain", message);
 }
 
-void handleLight() {
-  String payload = "lamps driver";
-  if(server.hasArg("bdr0")) {
-    int state = server.arg("bdr0").toInt();
+void handleLeds() {
+  String payload = "leds driver";
+
+  if(server.hasArg("kitchen")) {
+    int brightness = server.arg("kitchen").toInt();
+    if (brightness < 0) brightness = 0;
+    if (brightness > 1023) brightness = 1023;
+    kitchenPWM = brightness;
+    analogWrite(kitchenPin, brightness);
+    Serial.println("kitchen leds set");
+    payload = "kitchen leds set";
+  } else if(server.hasArg("bathroom")) {
+    int brightness = server.arg("bathroom").toInt();
+    if (brightness < 0) brightness = 0;
+    if (brightness > 1023) brightness = 1023;
+    bathroomPWM = brightness;
+    analogWrite(bathroomPin, brightness);
+    Serial.println("bathroom leds set");
+    payload = "bathroom leds set";
+  } else if(server.hasArg("rgb")) {
+    int state = server.arg("rgb").toInt();
     if (state == 0) {
-      digitalWrite(bdrPin0, HIGH);
+      digitalWrite(rgbPin, LOW);
+      Serial.println("rgb power off");
+      payload = "rgb power off";
     } else {
-      digitalWrite(bdrPin0, LOW);
+      digitalWrite(rgbPin, HIGH);
+      Serial.println("rgb power on");
+      payload = "rgb power on";
     }
-    Serial.println("Bedroom lamp0");
-  }
-  if(server.hasArg("bdr1")) {
-    int state = server.arg("bdr1").toInt();
+  } else if(server.hasArg("atx")) {
+    int state = server.arg("atx").toInt();
     if (state == 0) {
-      digitalWrite(bdrPin1, HIGH);
+      digitalWrite(atxPowerPin, HIGH);
+      Serial.println("atx power off");
+      payload = "atx power off";
     } else {
-      digitalWrite(bdrPin1, LOW);
-    }    Serial.println("Bedroom lamp1");
-  }
-  if(server.hasArg("bdr")) {
-    int state = server.arg("bdr").toInt();
-    if (state == 0) {
-      digitalWrite(bdrPin0, HIGH);
-      digitalWrite(bdrPin1, HIGH);
-    } else {
-      digitalWrite(bdrPin0, LOW);
-      digitalWrite(bdrPin1, LOW);
+      digitalWrite(atxPowerPin, LOW);
+      Serial.println("atx power on");
+      payload = "atx power on";
     }
-    Serial.println("Both bedroom lamps");
   }
-  if(server.hasArg("lvr0")) {
-    int state = server.arg("lvr0").toInt();
-    if (state == 0) {
-      digitalWrite(lvrPin0, HIGH);
-    } else {
-      digitalWrite(lvrPin0, LOW);
-    }
-    Serial.println("Living room lamp0");
+  else {
+    Serial.println("parameter not found");
+    payload = "parameter not found";
   }
-  if(server.hasArg("lvr1")) {
-    int state = server.arg("lvr1").toInt();
-    if (state == 0) {
-      digitalWrite(lvrPin1, HIGH);
-    } else {
-      digitalWrite(lvrPin1, LOW);
-    }
-    Serial.println("Living room lamp1");
-  }
-  if(server.hasArg("lvr")) {
-    int state = server.arg("lvr").toInt();
-    if (state == 0) {
-      digitalWrite(lvrPin0, HIGH);
-      digitalWrite(lvrPin1, HIGH);
-    } else {
-      digitalWrite(lvrPin0, LOW);
-      digitalWrite(lvrPin1, LOW);
-    }
-    Serial.println("Both living room lamps");
-  }
+
   Serial.println(payload);
   server.send(200, "text/plain", payload);
 }
